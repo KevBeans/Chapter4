3096eea156050302b2d12ab973f8c6d811c51dd6
Valter Melon <kevooi.of.estland@gmail.com>
2016-11-13 01:02:01 +0200

    completed

--- src/com/company/Ch42.java
+++ src/com/company/Ch42.java
@@ -2,7 +2,18 @@ package com.company;
 
 public class Ch42 {
 
+    public static void zool(int number, String pet, String street) {
+
+        System.out.println("Number: " + number);
+        System.out.println("Name of pet: " + pet);
+        System.out.println("Name of street: " + street);
+    }
+
     public static void main(String[] args) {
 
+        int number = 11;
+        String pet = "Uba";
+        String street = "Kivila";
+        zool(number, pet, street);
     }
 }
