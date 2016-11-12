634e4b8ee109f4f355ce9f4dd7b96d5a4e507b2d
Valter Melon <kevooi.of.estland@gmail.com>
2016-11-13 00:39:18 +0200

    comment updates

--- src/com/company/Ch41.java
+++ src/com/company/Ch41.java
@@ -2,10 +2,23 @@ package com.company;
 
 public class Ch41 {
 
-    //method ping adds "."
-    //method baffle adds "wug", followed by "."
-    //method zoop compounds ping and baffle, adds "You wugga" in between
-    //method main puts it all together
+    //method ping(); adds "."
+    //method baffle(); adds "wug", followed by "."
+    //method zoop(); compounds ping and baffle, adds "You wugga" in between
+    //method main(); puts it all together
+
+    /*
+    Stack diagram
+     ______________________________________________________
+    |ping   | "."                                          |
+    |baffle | "wug" / "."                                  |
+    |zoop   | "You wugga " / "wug" / "."                   |
+    |main   | "No, I " / "I " / "You wugga " / "wug" / "." |
+     ¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯
+    */
+
+    //Invoking baffle(); at the end of ping(); gives a stack overflow error
+
 
     public static void zoop() {
         baffle();
---
+++ src/com/company/Ch42.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Ch42 {
+
+    public static void main(String[] args) {
+
+    }
+}
